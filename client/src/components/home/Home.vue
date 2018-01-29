e5d713a971e13979c9dc2fe21083228f916ba199
Gustavo Soares <gustavo.soares@f1solucoes.com.br>
2018-01-29 14:25:24 -0200

    iniciando listagem de contatos

--- client/src/components/home/Home.vue
+++ client/src/components/home/Home.vue
@@ -9,19 +9,7 @@ import Listagem from '../listagem/Listagem.vue'
 export default {
     components: {
         'listagem': Listagem,
-        'contacts': [ ],
     },
-    created() {
-        this.$http.get('http://localhost:8081/contact/')
-            .then(res => {
-                res.json().then(contacts => {
-                    this.contacts = contacts
-                    console.log(contacts);
-                }, err => {
-                    console.log(err);
-                })
-            })
-    }
 }
 </script>
 
