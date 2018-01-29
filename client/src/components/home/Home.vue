14bed537b0fc089df08995a33d1581bd303f9403
Gustavo Soares <gustavo.soares@f1solucoes.com.br>
2018-01-29 11:10:44 -0200

    criando telas: Home e Listagem

---
+++ client/src/components/home/Home.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <listagem/>
+    </div>
+</template>
+
+<script>
+import Listagem from '../listagem/Listagem.vue'
+export default {
+    components: {
+        'listagem': Listagem,
+        'contacts': [ ],
+    },
+    created() {
+        this.$http.get('http://localhost:8081/contact/')
+            .then(res => {
+                res.json().then(contacts => {
+                    this.contacts = contacts
+                    console.log(contacts);
+                }, err => {
+                    console.log(err);
+                })
+            })
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
+
+
