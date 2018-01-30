376a2a43330aa99a2d2f82124f443893b7faa0d2
Gustavo Soares <gustavo10.fsoares@gmail.com>
2018-01-30 00:05:10 -0200

    extraindo template das pastas para limpar diretorio de arquivos

---
+++ client/src/components/Home.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <listagem/>
+    </div>
+</template>
+
+<script>
+import Listagem from './Listagem.vue'
+export default {
+    components: {
+        'listagem': Listagem,
+    },
+}
+</script>
+
+<style scoped>
+
+</style>
+
+
---
+++ client/src/components/Listagem.vue
@@ -0,0 +1,50 @@
+<template>
+    <div>
+        <table width="100%">
+            <tr>
+                <td>Nome</td>
+                <td>E-mail</td>
+                <td>Tel-1</td>
+                <td>Tel-2</td>
+                <td>Created</td>
+            </tr>
+            <tr v-for="contact of contacts">
+                <td>{{ contact.name }}</td>
+                <td>{{ contact.email }}</td>
+                <td>{{ contact.tel1 }}</td>
+                <td>{{ contact.tel2 }}</td>
+                <td>{{ contact.createdAt }}</td>
+            </tr>
+
+        </table>
+    </div>
+</template>
+
+<script>
+import { getContacts } from "../services/contacts"
+export default {
+
+    data() {
+        return {
+            contacts: [ ],
+        }
+    },
+    mounted() {
+        this.list()
+    },
+    methods: {
+        list() {
+            getContacts().then(res => { 
+                this.contacts = res.contacts 
+                console.log(res);
+            })
+        },
+    },
+}
+</script>
+
+<style scoped>
+
+</style>
+
+
