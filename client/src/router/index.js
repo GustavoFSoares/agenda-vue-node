592f98d53b38b4e78ad26237c7f3389a905e7b78
Gustavo Soares <gustavo10.fsoares@gmail.com>
2018-01-30 00:05:37 -0200

    extraindo '#' da url

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,7 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
-import Home from '@/components/home/Home.vue'
+import Home from '@/components/Home.vue'
+
 Vue.use(Router)
 
 export default new Router({
@@ -10,6 +11,13 @@ export default new Router({
       path: '/',
       name: 'Home',
       component: Home
-    }
-  ]
+    },
+    {
+      path: '/app',
+      name: 'HelloWorld',
+      component: HelloWorld
+    },
+
+  ],
+  mode: 'history'
 })
