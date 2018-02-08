d6536690ad189f95c72a30f623f918a53cb7a2bd
Gustavo Soares <gustavo.soares@f1solucoes.com.br>
2018-02-08 15:22:20 -0200

    iniciando html do curriculo

--- client/src/components/Resume.vue
+++ client/src/components/Resume.vue
@@ -3,6 +3,374 @@
         <h1>{{ title }}</h1>
 
         <h1 class="alert">Em Desenvolvimento!</h1>
+        
+        <!-- <div class="resume">
+            <div class="P14">
+                <span class="T1">GUSTAVO FERREIRA SOARES</span>
+            </div>
+            
+            <p class="P3">
+                <span class="T1">Brasileiro, solteiro, 20 anos
+                    <br/>CTPS: 1811889 Série: 0040/RS </span>
+            </p>
+            
+            <p class="P3">
+                <span class="T1">Título Eleitoral: 1131 1158 0442     Zona: 108    Seção: 0277</span>
+            </p>
+            
+            <p class="P3">
+                <span class="T1">Data de Nascimento: 07/08/1997</span>
+            </p>
+            
+            <p class="P3">
+                <span class="T1">Rua Gilberto Ferraz, n.º 105 - Jardim América - Bairro Lomba da Palmeira </span>
+            </p>
+            
+            <p class="P3">
+                <span class="T1">Sapucaia do Sul/RS - Cep: 93.225-070</span>
+            </p>
+            
+            <p class="P13">
+                <span class="T1">Fone(s):        (51)98470-0974</span>
+            </p>
+            
+            <p class="Cargo">
+                <span class="T2">E-mail: </span>
+                <a href="mailto:gustavo10.fsoares@gmail.com" class="Internet_20_link">
+                    <span class="Link_20_da_20_Internet">
+                        <span class="T2">gustavo10.fsoares@gmail.com</span>
+                    </span>
+                </a>
+            </p>
+            
+            <table border="0" cellspacing="0" cellpadding="0" class="Table1">
+                <colgroup>
+                    <col width="546" />
+                    <col width="184" />
+                </colgroup>
+
+                <tr class="Table11">
+                    <td colspan="2" style="text-align:left;width:12.492cm; " class="Table1_A1">
+                        <p class="P9">
+                            <span class="T4">Experiência</span>
+                        </p>
+                    </td>
+                </tr>
+
+                <tr class="Table12">
+                    <td colspan="2" style="text-align:left;width:12.492cm; " class="Table1_A1">
+                        <p class="P27">
+                            <span class="T3">2017         Estágio (12 meses) – F1 Soluções</span>
+                        </p>
+                        <p class="P16">
+                            <span class="T1">Função: Desenvolvedor PHP </span>
+                        </p>
+                        <p class="P17">
+                            <span class="T5">Principais atividades: Realizar coreções, modificações e inovações solicitas pelo cliente que que foram
+                                filtradas pela analista e encaminhadas para equipe de desenvolvimento.</span>
+                        </p>
+                    </td>
+                </tr>
+
+                <tr class="Table13">
+                    <td colspan="2" style="text-align:left;width:12.492cm; " class="Table1_A1">
+                        <p class="P27">
+                            <span class="T3">2014         Estágio (6 meses) – Secretaria Estadual do Trabalho, Cidadania e Assistência Social</span>
+                        </p>
+                        <p class="P16">
+                            <span class="T1">Função: Auxiliar em suporte técnico</span>
+                        </p>
+                        <p class="P18">
+                            <span class="T5">Principais atividades: Instalação e configuração de programas, atendimento ao usuário, instalação e configuração
+                                de impressoras, entre outras.</span>
+                        </p>
+                    </td>
+                </tr>
+
+                <tr class="Table11">
+                    <td colspan="2" style="text-align:left;width:12.492cm; " class="Table1_A1">
+                        <p class="P10">
+                            <span class="T4">Formação</span>
+                        </p>
+                    </td>
+                </tr>
+
+                <tr class="Table15">
+                    <td colspan="2" style="text-align:left;width:12.492cm; " class="Table1_A1">
+                        <p class="P28">
+                            <span class="T4">2014         </span>
+                            <span class="T3">Instituto Federal Sul Rio-Grandense - IFSul</span>
+                            <span class="T4">         </span>
+                            <span class="T3">Sapucaia do Sul/RS</span>
+                        </p>
+                        <p class="P19">
+                            <span class="T7">Técnico em</span>
+                            <span class="T1"> Informática, Curso de Educação Profissional Técnica de Nível Médio  – Forma Integrada – completo</span>
+                        </p>
+                    </td>
+                </tr>
+
+                <tr class="Table11">
+                    <td colspan="2" style="text-align:left;width:12.492cm; " class="Table1_A1">
+                        <p class="P20">
+                            <span class="T3">2013        EMEF Lourdes Fontoura da Silva        Sapucaia do Sul/RS</span>
+                        </p>
+                        <p class="P31">
+                            <span class="T1">Ensino Fundamental – completo        </span>
+                        </p>
+                        <p class="P21"> </p>
+                    </td>
+                </tr>
+
+                <tr class="Table11">
+                    <td colspan="2" style="text-align:left;width:12.492cm; " class="Table1_A1">
+                        <p class="P10">
+                            <span class="T4">cursos</span>
+                        </p>
+                    </td>
+                </tr>
+
+                <tr class="Table18">
+                    <td style="text-align:left;width:12.492cm; " class="Table1_A1">
+                        <p class="P29">
+                            <span class="T4">2017         ALURA – Cursos Online de Tecnologia</span>
+                        </p>
+                        <p class="P32">
+                            <a href="https://cursos.alura.com.br/user/SoaresGustavo/career/desenvolvedor-php-junior/certificate" class="Internet_20_link">
+                                <span class="Link_20_da_20_Internet">
+                                    <span class="T8">Iniciante Back-end PHP</span>
+                                </span>
+                            </a>
+                        </p>
+                        <h5 class="P6">
+                            <a id="a__HTML5_E_CSS3_I__Suas_Primeiras_Páginas_da_Web">
+                                <span/>
+                            </a>
+                            <a href="https://cursos.alura.com.br/certificate/6ebc071f383a9976ef62b9a3baea9cf2" class="Internet_20_link">
+                                <span class="Link_20_da_20_Internet">
+                                    <span class="T10">HTML5 E CSS3 I: </span>
+                                </span>
+                            </a>
+                            <a href="https://cursos.alura.com.br/certificate/6ebc071f383a9976ef62b9a3baea9cf2" class="Internet_20_link">
+                                <span class="Link_20_da_20_Internet">
+                                    <span class="T11">Suas Primeiras Páginas da We</span>
+                                </span>
+                            </a>
+                            <span class="T12">b</span>
+                        </h5>
+                        <p class="P32">
+                            <span class="T9">JavaScript: </span>
+                            <a href="https://cursos.alura.com.br/certificate/03e4f3bf30a2f09247e3932a03095c65"
+                                class="Internet_20_link">
+                                <span class="Link_20_da_20_Internet">
+                                    <span class="T8">Programando na Linguagem da Web</span>
+                                </span>
+                            </a>
+                        </p>
+                        <p class="P32">
+                            <span class="T9">jQuery: </span>
+                            <a href="https://cursos.alura.com.br/certificate/d7fdce56-3b1f-40f7-a912-ade6861698ca"
+                                class="Internet_20_link">
+                                <span class="Link_20_da_20_Internet">
+                                    <span class="T8">Manipulação Dinâmica de Conteúdo</span>
+                                </span>
+                            </a>
+                        </p>
+                        <p class="P32">
+                            <span class="T9">JAVA I e II: </span>
+                            <a href="https://cursos.alura.com.br/certificate/7ffb589c3e7b6bb087023cd634e28590"
+                                class="Internet_20_link">
+                                <span class="Link_20_da_20_Internet">
+                                    <span class="T8">Primeiros Passos e Orientação a Objetos</span>
+                                </span>
+                            </a>
+                        </p>
+                        <p class="P32">
+                            <span class="T9">Android I: </span>
+                            <a href="https://cursos.alura.com.br/certificate/41f18182-28fd-4c23-8112-49dfba5d575e"
+                                class="Internet_20_link">
+                                <span class="Link_20_da_20_Internet">
+                                    <span class="T8">Crie sua App Fantástica com Android Studio</span>
+                                </span>
+                            </a>
+                        </p>
+                        <p class="P32">
+                            <span class="T9">MySql II: </span>
+                            <a href="https://cursos.alura.com.br/certificate/c0bff6b7c7b69dab55ea67ffa7cf4e81" class="Internet_20_link">
+                                <span class="Link_20_da_20_Internet">
+                                    <span class="T8">Consultas Poderosas</span>
+                                </span>
+                            </a>
+                        </p>
+                        <p class="P32">
+                            <span class="T9">Linux I: </span>
+                            <a href="https://cursos.alura.com.br/certificate/34289bedb5d019be02e50e0b1c08c643" class="Internet_20_link">
+                                <span class="Link_20_da_20_Internet">
+                                    <span class="T8">Conhecendo e Utilizando o Terminal</span>
+                                </span>
+                            </a>
+                        </p>
+                    </td>
+                    <td style="text-align:left;width:4.202cm; " class="Table1_A1">
+                        <p class="P30">
+                            <span class="T4">Online</span>
+                        </p>
+                        <p class="P23">
+                            <span class="T1">(42 horas)</span>
+                        </p>
+                        <p class="P23">
+                            <span class="T1">(32 horas)</span>
+                        </p>
+                        <p class="P23">
+                            <span class="T1">(20 horas)</span>
+                        </p>
+                        <p class="P23">
+                            <span class="T1">(12 horas)</span>
+                        </p>
+                        <p class="P23">
+                            <span class="T1">(24 horas)</span>
+                        </p>
+                        <p class="P23">
+                            <span class="T1">(10 horas)</span>
+                        </p>
+                        <p class="P23">
+                            <span class="T1">(16 horas)</span>
+                        </p>
+                        <p class="P23">
+                            <span class="T1">(4 horas)</span>
+                        </p>
+                        <p class="P22"> </p>
+                    </td>
+                </tr>
+                <tr class="Table18">
+                    <td style="text-align:left;width:12.492cm; " class="Table1_A1">
+                        <p class="P29">
+                            <span class="T4">2015        Yázigi - Você cidadão do Mundo</span>
+                        </p>
+                        <p class="P24">
+                            <span class="T1">Inglês - Módulo MYP 1 - Cursando</span>
+                        </p>
+                    </td>
+                    <td style="text-align:left;width:4.202cm; " class="Table1_A1">
+                        <p class="P25">
+                            <span class="T3">Sapucaia do Sul/RS</span>
+                        </p>
+                    </td>
+                </tr>
+                <tr class="Table110">
+                    <td style="text-align:left;width:12.492cm; " class="Table1_A1">
+                        <p class="P29">
+                            <span class="T4">2012         UNIDIOMAS – Unilasalle Canoas</span>
+                        </p>
+                        <p class="P33">
+                            <span class="T1">1º Módulo do curso de Inglês</span>
+                        </p>
+                    </td>
+                    <td style="text-align:left;width:4.202cm; " class="Table1_A1">
+                        <p class="P30">
+                            <span class="T4">Canoas</span>
+                            <span class="T3">/RS</span>
+                        </p>
+                        <ul>
+                            <li>
+                                <p class="P26" style="margin-left:0cm;">
+                                    <span class="ListLabel_20_14" style="display:block;float:left;min-width:0,423cm;"></span>
+                                    <span class="odfLiEnd" /> </p>
+                            </li>
+                        </ul>
+                    </td>
+                </tr>
+                <tr class="Table111">
+                    <td style="text-align:left;width:12.492cm; " class="Table1_A1">
+                        <p class="P15">
+                            <span class="T3">2011        DataRed Formações Profissionais</span>
+                        </p>
+                        <p class="P33">
+                            <span class="T1">Montagem e Configuração de Computadores</span>
+                        </p>
+                    </td>
+                    <td style="text-align:left;width:4.202cm; " class="Table1_A1">
+                        <p class="P25">
+                            <span class="T3">Sapucaia do Sul/RS</span>
+                        </p>
+                    </td>
+                </tr>
+            </table>
+            <p class="P11_borderStart" />
+            <p class="P12_borderEnd">
+                <span class="T4">Outros Conhecimentos</span>
+            </p>
+            <table border="0" cellspacing="0" cellpadding="0" width="97%" class="Table2">
+                <colgroup>
+                    <col width="531" />
+                    <col width="189" />
+                </colgroup>
+                <tr class="Table21">
+                    <td style="text-align:left;width:12.151cm; " class="Table2_A1">
+                        <p class="P29">
+                            <span class="T4">        PHP</span>
+                        </p>
+                    </td>
+                    <td style="text-align:left;width:4.336cm; " class="Table2_A1">
+                        <p class="P30">
+                            <span class="T4">Júnior</span>
+                        </p>
+                    </td>
+                </tr>
+                <tr class="Table22">
+                    <td style="text-align:left;width:12.151cm; " class="Table2_A1">
+                        <p class="P29">
+                            <span class="T4">        MySQL</span>
+                        </p>
+                    </td>
+                    <td style="text-align:left;width:4.336cm; " class="Table2_A1">
+                        <p class="P30">
+                            <span class="T4">Júnior</span>
+                        </p>
+                    </td>
+                </tr>
+                <tr class="Table22">
+                    <td style="text-align:left;width:12.151cm; " class="Table2_A1">
+                        <p class="P29">
+                            <span class="T4">        JavaScript</span>
+                        </p>
+                    </td>
+                    <td style="text-align:left;width:4.336cm; " class="Table2_A1">
+                        <p class="P30">
+                            <span class="T4">Júnior</span>
+                        </p>
+                    </td>
+                </tr>
+                <tr class="Table22">
+                    <td style="text-align:left;width:12.151cm; " class="Table2_A1">
+                        <p class="P29">
+                            <span class="T4">        jQuery</span>
+                        </p>
+                    </td>
+                    <td style="text-align:left;width:4.336cm; " class="Table2_A1">
+                        <p class="P30">
+                            <span class="T4">Júnior</span>
+                        </p>
+                    </td>
+                </tr>
+                <tr class="Table22">
+                    <td style="text-align:left;width:12.151cm; " class="Table2_A1">
+                        <p class="P29">
+                            <span class="T4">        JAVA</span>
+                        </p>
+                    </td>
+                    <td style="text-align:left;width:4.336cm; " class="Table2_A1">
+                        <p class="P30">
+                            <span class="T4">Básico</span>
+                        </p>
+                    </td>
+                </tr>
+            </table>
+            <p class="P5"> </p>
+            <p class="P4">
+                <span class="T1">Sapucaia do Sul, 10 de Janeiro de 2018.</span>
+            </p>
+        </div> -->
     </div>
 
 </template>
