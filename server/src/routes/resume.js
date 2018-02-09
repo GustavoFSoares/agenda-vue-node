ce31992e61012f38bbb7d9025f1aa74254a28c1f
Gustavo Soares <gustavo.soares@f1solucoes.com.br>
2018-02-09 14:19:47 -0200

    adicionando rodas de edicao e exclusao

--- server/src/routes/resume.js
+++ server/src/routes/resume.js
@@ -8,25 +8,42 @@ router.get('/', async (req, res, next) => {
     try {
         res.status(200).json(await db.resume().getResume());
     } catch (err) {
-        res.status(400).json(await { msg: 'Contatos não encontrados', data_error: err });
+        res.status(400).json(await { msg: 'Currículo não encontrados', data_error: err });
     }
     next()
 });
 
-router.post('/', async (req, res, next) => {
+router.post('/add', async (req, res, next) => {
     try {
         res.status(200).json(await db.resume().save(req.body));
     } catch (err) {
-        res.status(400).json(await { msg: 'Contatos não encontrados', data_error: err });
+        res.status(400).json(await { msg: 'Curriculo não cadastrado', data_error: err });
     }
     next()
 });
+
+router.put('/edit/:id', async (req, res, next) => {
+    const { id } = req.params
+    try {
+        res.status(200).json(await db.resume().update(id, req.body))
+    } catch (err) {
+        res.status(400).json(await { msg: "Currículo não atualizado" })
+    }
+})
+
+router.post('/delete/:id', async (req, res, next) => {
+    try {
+        
+    } catch (err) {
+        
+    }
+})
 
 router.get('/insert', async (req, res, next) => {   
     try {
         res.status(200).json(await db.resume().save(default_resume.resume));
     } catch (err) {
-        res.status(400).json(await { msg: 'Contatos não encontrados', data_error: err });
+        res.status(400).json(await { msg: 'Curriculo não cadastrado', data_error: err });
     }
     next()
 });
