f8d86c919c789ab44a0dcec6fcfb2fd17e749eb8
Gustavo Soares <gustavo.soares@f1solucoes.com.br>
2018-02-09 13:39:00 -0200

    inserindo curriculo default

--- server/src/routes/resume.js
+++ server/src/routes/resume.js
@@ -1,9 +1,34 @@
 const express = require('express');
 const db = require('../services/mongodb');
+const default_resume = require('../resume-default')
 
 const router = express.Router();
 
 router.get('/', async (req, res, next) => {
-    res.send('ok')
+    try {
+        res.status(200).json(await db.resume().getResume());
+    } catch (err) {
+        res.status(400).json(await { msg: 'Contatos não encontrados', data_error: err });
+    }
+    next()
 });
+
+router.post('/', async (req, res, next) => {
+    try {
+        res.status(200).json(await db.resume().save(req.body));
+    } catch (err) {
+        res.status(400).json(await { msg: 'Contatos não encontrados', data_error: err });
+    }
+    next()
+});
+
+router.get('/insert', async (req, res, next) => {   
+    try {
+        res.status(200).json(await db.resume().save(default_resume.resume));
+    } catch (err) {
+        res.status(400).json(await { msg: 'Contatos não encontrados', data_error: err });
+    }
+    next()
+});
+
 module.exports = app => app.use('/resume', router);
